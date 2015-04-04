f87d260fab4f5766a4864c25c8e18c3c31bae701
Thiago Braga <contato@thiagobraga.org>
2015-04-03 22:10:24 -0300

    Correcting environment URL

--- application/config/testing/config.php
+++ application/config/testing/config.php
@@ -15,7 +15,7 @@
  * If this is not set then CodeIgniter will guess the protocol, domain and
  * path to your installation.
  */
-$config['base_url'] = 'http://testing.escapefromtheworld.com';
+$config['base_url'] = 'http://test.escapefromtheworld.com';
 
 /**
  * -----------------------------------------------------------------------------
